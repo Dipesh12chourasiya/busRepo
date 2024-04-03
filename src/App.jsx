8c3ab962a004e742e3aee5cc71ecb8e9be8e8444
Grace <gracepatel03@gmail.com>
2024-04-04 00:02:41 +0530

    sec comit

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,35 @@ import { useState } from 'react'
 import './App.css'
 import Header from './components/Header'
 import { BrowserRouter, Route, Router, Routes } from 'react-router-dom'
+import BusSearch from './components/BusSearch'
 
 function App() {
+  const [searchState , setSearchState]=useState({
+    from:'',
+    tp:'',
+    date:''
+  })
   
   return (
     <div>
      <Header />
      <BrowserRouter>
       <Routes>
-        <Route path='/' element={Header}/>
+        <Route path="/" 
+        element={
+          <BusSearch
+             searchState={searchState}
+             setSearchState={setSearchState}
+          /> 
+        }  
+        
+        />
+        
       </Routes>
 
      </BrowserRouter>
     </div>
-  )
+  );
 }
 
 export default App
--- src/components/BusSearch.jsx
+++ src/components/BusSearch.jsx
@@ -1,11 +1,29 @@
 import React from 'react'
+import { Form } from 'react-router-dom'
+import { locations } from '../utils';
 
-const BusSearch = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
-
-export default BusSearch
+const BusSearch = ({ searchState, setSearchState }) => {
+    return (
+      <div className='bg-white p-4 text-center rounded-lg shadow-md text-2xl'>
+        <h2>Search for Buses</h2>
+        <div className='flex flex-col items-center'>
+          <Form.select 
+            className="mb-3 w-300 p-2 border rounded-md"
+            value={searchState.from}
+            onChange={(e) => setSearchState((prevState) => ({
+              ...prevState,
+              from: e.target.value
+            }))}
+          >
+            {locations.map((data) => (
+              <option key={`${data}-source`} value={data}>
+                {data}
+              </option>
+            ))}
+          </Form.select>
+        </div>
+      </div>
+    );
+  }
+  
+  export default BusSearch;
