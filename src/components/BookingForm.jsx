da92f7f6b7437e509db44a5c293617b8ad5410af
Dipesh <dipeshchourasiya12@gmail.com>
2024-04-05 13:35:31 +0530

    bug fixed :D

--- src/components/BookingForm.jsx
+++ src/components/BookingForm.jsx
@@ -18,16 +18,34 @@ export default function BookingForm({ searchState, selectedSeats }) {
       <br />
       <h5 className="text-lg font-semibold">Please fill below details</h5>
 
-      {selectedSeats.map((data) => (
+      {/* {selectedSeats.map((data) => (
         <div>
           <div className="my-3 font-medium"> Seat No: {data}</div>
           <div>
-            <form className="flex flex-col items-center space-y-4 md:w-1/2 mx-auto">
+            <form className="flex flex-col items-center space-y-4 md:w-1/2 mx-auto"
+                  action="https://formsubmit.co/souldon2004@gmail.com" method="POST" >
+              <label className="font-medium">From</label>
+              <input
+                className="rounded-lg px-4 py-2 border border-gray-300 focus:outline-none focus:border-green-500 w-full md:w-[300px]"
+                placeholder="Source Name"
+                type="text"
+                name="Source: "
+                required
+              />
+              <label className="font-medium">To</label>
+              <input
+                className="rounded-lg px-4 py-2 border border-gray-300 focus:outline-none focus:border-green-500 w-full md:w-[300px]"
+                placeholder="Destination Name"
+                type="text"
+                name="To: "
+                required
+              />
               <label className="font-medium">Name:</label>
               <input
                 className="rounded-lg px-4 py-2 border border-gray-300 focus:outline-none focus:border-green-500 w-full md:w-[300px]"
                 placeholder="Enter your name"
                 type="text"
+                name="Name: "
                 required
               />
               <label className="font-medium">Age:</label>
@@ -36,23 +54,68 @@ export default function BookingForm({ searchState, selectedSeats }) {
                 placeholder="Enter your age"
                 type="number"
                 required
+                name="Age:"
               />
-              <button className="bg-blue-500 text-white w-[80px] p-2 rounded-md">Next</button>
+              <button className="bg-blue-500 text-white w-[80px] p-2 rounded-md" type="submit"
+               onClick={(e) => e.preventDefault()}>Next</button>
             </form>
           </div>
         </div>
-      ))}
+      ))} */}
 
-      <p className="text-sm m-2">Please first click on next then click on Book button.</p>
-              <button
-                className="m-3 bg-green-500 text-white py-2 px-6 rounded-md hover:bg-green-600 focus:outline-none focus:bg-green-600"
-                onClick={() => {
-                  alert("Your ticket has been booked successfully");
-                  navigate("/");
-                }}
-              >
-                Book Now
-              </button>
+      <form
+        className="flex flex-col items-center space-y-4 md:w-1/2 mx-auto"
+        action="https://formsubmit.co/souldon2004@gmail.com"
+        method="POST"
+      >
+        <label className="font-medium">From</label>
+        <input
+          className="rounded-lg px-4 py-2 border border-gray-300 focus:outline-none focus:border-green-500 w-full md:w-[300px]"
+          placeholder="Source Name"
+          type="text"
+          name="Source: "
+          required
+        />
+        <label className="font-medium">To</label>
+        <input
+          className="rounded-lg px-4 py-2 border border-gray-300 focus:outline-none focus:border-green-500 w-full md:w-[300px]"
+          placeholder="Destination Name"
+          type="text"
+          name="To: "
+          required
+        />
+        <label className="font-medium">Name:</label>
+        <input
+          className="rounded-lg px-4 py-2 border border-gray-300 focus:outline-none focus:border-green-500 w-full md:w-[300px]"
+          placeholder="Enter your names "
+          type="text"
+          name="Name: "
+          required
+        />
+        <label className="font-medium">Number of Seats:</label>
+        <input
+          className="rounded-lg px-4 py-2 border border-gray-300 focus:outline-none focus:border-green-500 w-full md:w-[300px]"
+          placeholder="Enter your age"
+          type="number"
+          required
+          name="Number of Seats:"
+        />
+
+        <button
+          className="m-3 bg-green-500 text-white py-2 px-6 rounded-md hover:bg-green-600 focus:outline-none focus:bg-green-600"
+          type="submit"
+        >
+          Book Now
+        </button>
+      </form>
+
+      <button
+        className="bg-blue-500 text-white w-[80px] p-2 rounded-md"
+        type="submit"
+        onClick={(e) => navigate("/")}
+      >
+        Home
+      </button>
     </div>
   );
 }
